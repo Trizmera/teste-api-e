26bdc1a51b19ed36588efa173bab9078e3cd098e
Beatriz Cruz <beatrizbbcruz@gmail.com>
2025-03-14 11:54:31 -0300

    inicio do projeto com spring initializr

---
+++ src/main/java/com/trizmera/teste_api_e/TesteApiEApplication.java
@@ -0,0 +1,13 @@
+package com.trizmera.teste_api_e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteApiEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteApiEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trizmera/teste_api_e/TesteApiEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trizmera.teste_api_e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApiEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
